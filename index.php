15d1892a116dc46c28bff1445ddee3389afa4881
Aladser <aladser@gmail.com>
2022-12-11 13:35:48 +0900

    мелкие правки

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'perfect-partner-search.php'; ?>
-<?php include 'data.php'; ?>
+<?php include 'scriptes/perfect-partner-search.php'; ?>
+<?php include 'resources/data.php'; ?>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+    <link rel="shortcut icon" href="resources/icon.ico">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Идеальный подбор пары</title>
 </head>
 <body>
     <header class='header'> <h2>Идеальный подбор пары</h2></header>
